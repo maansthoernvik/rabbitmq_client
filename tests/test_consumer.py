c205599be928dcbd36fec4c83e4457ed0e6d1a8b
Måns Thörnvik <thornvik.mans@gmail.com>
2021-05-22 22:37:18 +0200

    Finalize RMQConnection handling of consume calls
    Implement handling for further calls when consume is called, depending
    on input. Implement handling for on_ready and on_close in terms of
    what should happen to existing consumes.

--- tests/test_consumer.py
+++ tests/test_consumer.py
@@ -1,17 +1,9 @@
 import unittest
 
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 
 from rabbitmq_client import ExchangeParams, ConsumeParams, QueueParams
 from rabbitmq_client.new_consumer import RMQConsumer, _gen_consume_key
-
-
-def queue(name):
-    return QueueParams(name)
-
-
-def exchange(name):
-    return ExchangeParams(name)
 
 
 class TestConsumeKeyGeneration(unittest.TestCase):
@@ -21,25 +13,27 @@ class TestConsumeKeyGeneration(unittest.TestCase):
         self.assertEqual(
             "queue|exchange|routing_key",
             _gen_consume_key(
-                queue("queue"), exchange("exchange"), "routing_key"
+                QueueParams("queue"), ExchangeParams("exchange"), "routing_key"
             )
         )
         self.assertEqual(
             "exchange|routing_key",
             _gen_consume_key(
-                None, exchange("exchange"), "routing_key"
+                None, ExchangeParams("exchange"), "routing_key"
             )
         )
         self.assertEqual(
             "queue|routing_key",
             _gen_consume_key(
-                queue("queue"), None, "routing_key"
+                QueueParams("queue"), None, "routing_key"
             )
         )
         self.assertEqual(
             "queue|exchange|complex.routing.key",
             _gen_consume_key(
-                queue("queue"), exchange("exchange"), "complex.routing.key"
+                QueueParams("queue"),
+                ExchangeParams("exchange"),
+                "complex.routing.key"
             )
         )
 
@@ -65,14 +59,15 @@ class TestConsumer(unittest.TestCase):
         exchange = ExchangeParams(
             "exchange"
         )
-        self.consumer.consume(consume, exchange=exchange)
+        self.consumer.consume(consume, exchange_params=exchange)
 
         # A single consume should exist, that is not confirmed
-        consume_key = _gen_consume_key(exchange=exchange)
-        consume_instance = self.consumer._running_consumes[consume_key]
+        consume_key = _gen_consume_key(exchange_params=exchange)
+        consume_instance = self.consumer._consumes[consume_key]
 
-        self.assertEqual(consume_instance.consume, consume)
-        self.assertEqual(consume_instance.exchange, exchange)
+        # Assertions
+        self.assertEqual(consume_instance.consume_params, consume)
+        self.assertEqual(consume_instance.exchange_params, exchange)
 
     def test_consume_queue_only(self):
         """Verify possibility to consume from a queue."""
@@ -83,14 +78,15 @@ class TestConsumer(unittest.TestCase):
         queue = QueueParams(
             "queue"
         )
-        self.consumer.consume(consume, queue=queue)
+        self.consumer.consume(consume, queue_params=queue)
 
         # A single consume should exist, that is not confirmed
-        consume_key = _gen_consume_key(queue=queue)
-        consume_instance = self.consumer._running_consumes[consume_key]
+        consume_key = _gen_consume_key(queue_params=queue)
+        consume_instance = self.consumer._consumes[consume_key]
 
-        self.assertEqual(consume_instance.consume, consume)
-        self.assertEqual(consume_instance.queue, queue)
+        # Assertions
+        self.assertEqual(consume_instance.consume_params, consume)
+        self.assertEqual(consume_instance.queue_params, queue)
 
     def test_consume_both_queue_and_exchange(self):
         """
@@ -107,15 +103,20 @@ class TestConsumer(unittest.TestCase):
         exchange = ExchangeParams(
             "exchange"
         )
-        self.consumer.consume(consume, queue=queue, exchange=exchange)
+        self.consumer.consume(
+            consume, queue_params=queue, exchange_params=exchange
+        )
 
         # A single consume should exist, that is not confirmed
-        consume_key = _gen_consume_key(queue=queue, exchange=exchange)
-        consume_instance = self.consumer._running_consumes[consume_key]
+        consume_key = _gen_consume_key(
+            queue_params=queue, exchange_params=exchange
+        )
+        consume_instance = self.consumer._consumes[consume_key]
 
-        self.assertEqual(consume_instance.consume, consume)
-        self.assertEqual(consume_instance.queue, queue)
-        self.assertEqual(consume_instance.exchange, exchange)
+        # Assertions
+        self.assertEqual(consume_instance.consume_params, consume)
+        self.assertEqual(consume_instance.queue_params, queue)
+        self.assertEqual(consume_instance.exchange_params, exchange)
 
     def test_consume_queue_exchange_and_routing_key(self):
         """
@@ -133,21 +134,27 @@ class TestConsumer(unittest.TestCase):
             "exchange"
         )
         self.consumer.consume(
-            consume, queue=queue, exchange=exchange, routing_key="routing_key"
+            consume,
+            queue_params=queue,
+            exchange_params=exchange,
+            routing_key="routing_key"
         )
 
         # A single consume should exist, that is not confirmed
         consume_key = _gen_consume_key(
-            queue=queue, exchange=exchange, routing_key="routing_key"
+            queue_params=queue,
+            exchange_params=exchange,
+            routing_key="routing_key"
         )
-        consume_instance = self.consumer._running_consumes[consume_key]
+        consume_instance = self.consumer._consumes[consume_key]
 
-        self.assertEqual(consume_instance.consume, consume)
-        self.assertEqual(consume_instance.queue, queue)
-        self.assertEqual(consume_instance.exchange, exchange)
+        # Assertions
+        self.assertEqual(consume_instance.consume_params, consume)
+        self.assertEqual(consume_instance.queue_params, queue)
+        self.assertEqual(consume_instance.exchange_params, exchange)
         self.assertEqual(consume_instance.routing_key, "routing_key")
 
-    def test_neither_queue_nor_exchange_provided(self):
+    def test_consume_neither_queue_nor_exchange_provided(self):
         """
         Verify that consume raises an exception if neither queue nor exchange
         is provided.
@@ -155,7 +162,7 @@ class TestConsumer(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.consumer.consume(ConsumeParams(lambda _: ...))
 
-    def test_consume_same_consume_key_raises_exception(self):
+    def test_consume_same_consume_key(self):
         """
         Verify that calling consume once more with the same queue name,
         exchange name and routing key will result in an exception.
@@ -163,7 +170,154 @@ class TestConsumer(unittest.TestCase):
         self.consumer.consume(
             ConsumeParams(lambda _: ...), QueueParams("queue")
         )
+
+        # Assertions
         with self.assertRaises(ValueError):
             self.consumer.consume(
                 ConsumeParams(lambda _: ...), QueueParams("queue")
             )
+
+    def test_on_close_marks_consumes_stopped(self):
+        """Verify a call to on_close marks all consumes as stopped."""
+        queue_consume = ConsumeParams(lambda _: ...)
+        queue = QueueParams("queue")
+        self.consumer.consume(queue_consume, queue_params=queue)
+
+        exchange_consume = ConsumeParams(lambda _: ...)
+        exchange = ExchangeParams("exchange")
+        self.consumer.consume(exchange_consume, exchange_params=exchange)
+
+        # Fake having all consumes started
+        for _, consume in self.consumer._consumes.items():
+            consume.consumer_tag = "some tag"
+
+        # Run test
+        self.consumer.on_close()
+
+        # Checks
+        for _, consume in self.consumer._consumes.items():
+            if consume.consumer_tag == "some tag":
+                self.fail(
+                    "a consume did not have its consumer tag cleared when "
+                    "on_close was called"
+                )
+
+    def test_consume_when_ready_queue_only(self):
+        """
+        Verify work is sent to connection if consumer is ready when consume
+        is called.
+        """
+        # Set up
+        self.consumer.declare_queue = Mock()
+        self.consumer.on_ready()  # Connection reports it is ready
+
+        # Run test
+        consume_params = ConsumeParams(lambda _: ...)
+        queue_params = QueueParams("queue")
+        self.consumer.consume(
+            consume_params, queue_params=queue_params
+        )
+
+        # Assertions
+        self.consumer.declare_queue.assert_called_with(
+            queue_params, consume_params=consume_params
+        )
+
+    def test_consume_when_ready_exchange_only(self):
+        """
+        Verify work is sent to connection if consumer is ready when consume
+        is called.
+        """
+        # Set up
+        self.consumer.declare_exchange = Mock()
+        self.consumer.on_ready()  # Connection reports it is ready
+
+        # Run test
+        consume_params = ConsumeParams(lambda _: ...)
+        exchange_params = ExchangeParams("exchange")
+        self.consumer.consume(
+            consume_params, exchange_params=exchange_params
+        )
+
+        # Assertions
+        self.consumer.declare_exchange.assert_called_with(
+            exchange_params,
+            queue_params=None,
+            routing_key=None,
+            consume_params=consume_params
+        )
+
+    def test_consume_when_ready_both_queue_and_exchange(self):
+        """
+        Verify work is sent to connection if consumer is ready when consume
+        is called.
+        """
+        # Set up
+        self.consumer.declare_exchange = Mock()
+        self.consumer.on_ready()  # Connection reports it is ready
+
+        # Run test
+        consume_params = ConsumeParams(lambda _: ...)
+        exchange_params = ExchangeParams("exchange")
+        queue_params = QueueParams("queue")
+        self.consumer.consume(
+            consume_params,
+            queue_params=queue_params,
+            exchange_params=exchange_params
+        )
+
+        # Assertions
+        self.consumer.declare_exchange.assert_called_with(
+            exchange_params,
+            queue_params=queue_params,
+            routing_key=None,
+            consume_params=consume_params
+        )
+
+    def test_consume_before_ready_does_nothing(self):
+        """
+        Verify that if consume is called before the consumer is ready, nothing
+        happens.
+        """
+        # Set up
+        self.consumer.declare_queue = Mock()
+
+        # Run test
+        self.consumer.consume(
+            ConsumeParams(lambda _: ...), queue_params=QueueParams("queue")
+        )
+
+        # Assertions
+        self.consumer.declare_queue.assert_not_called()
+
+    def test_on_ready_starts_consumes(self):
+        """
+        Verify a call to on_ready will lead to all consumes being re-issued.
+        """
+        # Set up
+        self.consumer.declare_queue = Mock()
+        self.consumer.declare_exchange = Mock()
+
+        queue_consume = ConsumeParams(lambda _: ...)
+        queue_params = QueueParams("queue")
+        self.consumer.consume(queue_consume, queue_params=queue_params)
+
+        exchange_consume = ConsumeParams(lambda _: ...)
+        exchange_params = ExchangeParams("exchange")
+        self.consumer.consume(
+            exchange_consume, exchange_params=exchange_params
+        )
+
+        # Run test
+        self.consumer.on_ready()  # Now the connection is ready!
+
+        # Assertions
+        self.consumer.declare_queue.assert_called_with(
+            queue_params, consume_params=queue_consume
+        )
+        self.consumer.declare_exchange.assert_called_with(
+            exchange_params,
+            queue_params=None,
+            routing_key=None,
+            consume_params=exchange_consume
+        )
