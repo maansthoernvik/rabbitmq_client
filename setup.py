a340bd24045808b0f87c68befba8248b131a8b23
Måns Thörnvik <thornvik.mans@gmail.com>
2022-03-06 16:48:50 +0100

    Release 2.1.0
    2.1.0 adds ack mode to the consume interface.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/maansthoernvik/rabbitmq_client'
 EMAIL = 'thornvik.mans@gmail.com'
 AUTHOR = 'Måns Thörnvik'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '2.0.3'
+VERSION = '2.1.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
