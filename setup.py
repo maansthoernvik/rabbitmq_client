6fa771ce09809cd0b3e551db7965392e95240a5b
Måns Thörnvik <thornvik.mans@gmail.com>
2022-04-17 16:40:36 +0200

    Release 2.3.0
    Mandatory publishes, coverage, fixes.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/maansthoernvik/rabbitmq_client'
 EMAIL = 'thornvik.mans@gmail.com'
 AUTHOR = 'Måns Thörnvik'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '2.2.0'
+VERSION = '2.3.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
